b1ad5feefb15057daec134ee191eda4d75777ae3
Konstantin Tarkus <hello@tarkus.me>
2012-12-07 20:08:41 +0400

    Update Tick db table

--- src/Database/dbo/Tables/Tick.sql
+++ src/Database/dbo/Tables/Tick.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Tick] (
-    [SecurityID] INT          NOT NULL,
-    [Date]       DATETIME2(2) NOT NULL,
-    [Price]      SMALLMONEY   NOT NULL,
-    [Volume]     INT          NOT NULL,
+    [SecurityID] INT           NOT NULL,
+    [Date]       DATETIME2 (7) NOT NULL,
+    [Price]      SMALLMONEY    NOT NULL,
+    [Volume]     INT           NOT NULL,
     CONSTRAINT [PK_Tick_SecurityID_Date] PRIMARY KEY CLUSTERED ([SecurityID] ASC, [Date] DESC),
     CONSTRAINT [FK_Tick_Security] FOREIGN KEY ([SecurityID]) REFERENCES [dbo].[Security] ([SecurityID])
 );
